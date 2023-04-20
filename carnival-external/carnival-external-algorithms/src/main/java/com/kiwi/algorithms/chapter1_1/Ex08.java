53573b923f93e8e701ddf1496f40ead03b97fb13
kiwi4814 <m484@qq.com>
2023-04-20 11:00:00 +0800

    chapter1_1 ex08~ex11

---
+++ carnival-external/carnival-external-algorithms/src/main/java/com/kiwi/algorithms/chapter1_1/Ex08.java
@@ -0,0 +1,9 @@
+package com.kiwi.algorithms.chapter1_1;
+
+public class Ex08 {
+	public static void main(String[] args) {
+		System.out.println('b');
+		System.out.println('b' + 'c');
+		System.out.println((char) ('a' + 4));
+	}
+}
